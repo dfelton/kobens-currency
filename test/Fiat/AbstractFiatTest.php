c24a6cdce074d24dc444d943654cafe638aa6878
Darren Felton <darren_felton@hotmail.com>
2019-02-19 08:21:02 -0600

    Added AbstractFiat tests

---
+++ test/Fiat/AbstractFiatTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace KobensTest\Currency\Fiat;
+
+use PHPUnit\Framework\TestCase;
+use Kobens\Currency\Fiat\AbstractFiat;
+use Kobens\Currency\Exception\InvalidConstantException;
+
+class AbstractFiatTest extends TestCase
+{
+    /**
+     * @var AbstractFiat
+     */
+    protected $mock;
+
+    public function setup() : void
+    {
+        $this->mock = $this->getMockForAbstractClass(AbstractFiat::class);
+    }
+
+    public function testGetCurrencyType() : void
+    {
+        $this->assertEquals('fiat', $this->mock->getCurrencyType());
+    }
+
+    public function testGetCurrencyNameThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getCurrencyName();
+    }
+
+    public function testGetMainUnitNameThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getMainUnitName();
+    }
+
+    public function testGetMainUnitAbbreviationThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getMainUnitAbbreviation();
+    }
+
+    public function testGetSubunitNameThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getSubunitName();
+    }
+
+    public function testGetSubunitDenominationThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getSubunitDenomination();
+    }
+
+    public function testGetCacheIdentifierThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getCacheIdentifier();
+    }
+
+    public function testGetPairIdentityThrowsException() : void
+    {
+        $this->expectException(InvalidConstantException::class);
+        $this->mock->getPairIdentity();
+    }
+
+}
