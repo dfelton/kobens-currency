62039af64685a12bde1c1cb548c56c299843964c
Darren Felton <2659558+dfelton@users.noreply.github.com>
2019-02-18 22:13:31 -0600

    Added Exceptions; Throw Exceptions; Final; static not self

--- src/AbstractCurrency.php
+++ src/AbstractCurrency.php
@@ -2,6 +2,8 @@
 
 namespace Kobens\Currency;
 
+use \Kobens\Currency\Exception\InvalidConstantException;
+
 abstract class AbstractCurrency implements CurrencyInterface
 {
     const CURRENCY_TYPE          = null;
@@ -12,45 +14,115 @@ abstract class AbstractCurrency implements CurrencyInterface
     const DENOMINATION           = null;
     const PAIR_IDENTIFIER        = null;
     const CACHE_IDENTIFIER       = null;
-    
-    public function getCurrencyType() : string
+
+    final public function getCurrencyType() : string
     {
-        return self::CURRENCY_TYPE;
+        if (   \is_string(static::CURRENCY_TYPE) === false
+            || \strlen(static::CURRENCY_TYPE) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'CURRENCY_TYPE'
+            ));
+        }
+        return static::CURRENCY_TYPE;
     }
-    
-    public function getCurrencyName() : string
+
+    final public function getCurrencyName() : string
     {
-        return self::CURRENCY_NAME;
+        if (   \is_string(static::CURRENCY_NAME) === false
+            || \strlen(static::CURRENCY_NAME) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'CURRENCY_NAME'
+            ));
+        }
+        return static::CURRENCY_NAME;
     }
-    
-    public function getMainUnitAbbreviation() : string
+
+    final public function getMainUnitAbbreviation() : string
     {
-        return self::MAIN_UNIT_ABBREVIATION;
+        if (   \is_string(static::MAIN_UNIT_ABBREVIATION) === false
+            || \strlen(static::MAIN_UNIT_ABBREVIATION) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'MAIN_UNIT_ABBREVIATION'
+            ));
+        }
+        return static::MAIN_UNIT_ABBREVIATION;
     }
-    
-    public function getMainUnitName() : string
+
+    final public function getMainUnitName() : string
     {
-        return self::MAIN_UNIT;
+        if (   \is_string(static::MAIN_UNIT) === false
+            || \strlen(static::MAIN_UNIT) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'MAIN_UNIT'
+            ));
+        }
+        return static::MAIN_UNIT;
     }
-    
-    public function getSubunitName() : string
+
+    final public function getSubunitName() : string
     {
-        return self::SUB_UNIT;
+        if (   \is_string(static::SUB_UNIT) === false
+            || \strlen(static::SUB_UNIT) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'SUB_UNIT'
+            ));
+        }
+        return static::SUB_UNIT;
     }
-    
-    public function getSubunitDenomination() : int
+
+    final public function getSubunitDenomination() : int
     {
-        return self::DENOMINATION;
+        if (\is_int(static::DENOMINATION) === false) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be an integer',
+                static::class,
+                'DENOMINATION'
+            ));
+        }
+        return static::DENOMINATION;
     }
-    
-    public function getPairIdentity() : string
+
+    final public function getPairIdentity() : string
     {
-        return self::PAIR_IDENTIFIER;
+        if (   \is_string(static::PAIR_IDENTIFIER) === false
+            || \strlen(static::PAIR_IDENTIFIER) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'PAIR_IDENTIFIER'
+            ));
+        }
+        return static::PAIR_IDENTIFIER;
     }
-    
-    public function getCacheIdentifier() : string
+
+    final public function getCacheIdentifier() : string
     {
-        return self::CACHE_IDENTIFIER;
+        if (   \is_string(static::CACHE_IDENTIFIER) === false
+            || \strlen(static::CACHE_IDENTIFIER) === 0
+        ) {
+            throw new InvalidConstantException(\sprintf(
+                '%s::%s must be a string with a length greater than zero',
+                static::class,
+                'CACHE_IDENTIFER'
+            ));
+        }
+        return static::CACHE_IDENTIFIER;
     }
-    
+
 }
