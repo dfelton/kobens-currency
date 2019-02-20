8fc6fc118816a121c618d4d28c349921b4aca69b
Darren Felton <darren_felton@hotmail.com>
2019-02-19 20:11:54 -0600

    Added USDTest

---
+++ test/Fiat/USDTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace KobensTest\Currency\Fiat;
+
+use PHPUnit\Framework\TestCase;
+use Kobens\Currency\Fiat\USD;
+
+class USDTest extends TestCase
+{
+    /**
+     * @var USD
+     */
+    protected $testClass;
+
+    public function setup() : void
+    {
+        $this->testClass = new USD();
+    }
+
+    public function testGetCurrencyType() : void
+    {
+        $this->assertEquals('fiat', $this->testClass->getCurrencyType());
+    }
+
+    public function testGetCurrencyName() : void
+    {
+        $this->assertEquals('US Dollar', $this->testClass->getCurrencyName());
+    }
+
+    public function testGetMainUnitName() : void
+    {
+        $this->assertEquals('Dollar', $this->testClass->getMainUnitName());
+    }
+
+    public function testGetMainUnitAbbreviation() : void
+    {
+        $this->assertEquals('USD', $this->testClass->getMainUnitAbbreviation());
+    }
+
+    public function testGetSubunitName() : void
+    {
+        $this->assertEquals('Cent', $this->testClass->getSubunitName());
+    }
+
+    public function testGetSubunitDenomination() : void
+    {
+        $this->assertEquals(2, $this->testClass->getSubunitDenomination());
+    }
+
+    public function testGetCacheIdentifier() : void
+    {
+        $this->assertEquals('usd', $this->testClass->getCacheIdentifier());
+    }
+
+    public function testGetPairIdentity() : void
+    {
+        $this->assertEquals('usd', $this->testClass->getPairIdentity());
+    }
+
+}
