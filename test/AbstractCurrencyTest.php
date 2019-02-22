8d16a4b6ccfcdade2fcaf594f2894cee6a79880b
Darren Felton <2659558+dfelton@users.noreply.github.com>
2019-02-21 19:18:31 -0600

    Anonymous class instead

--- test/AbstractCurrencyTest.php
+++ test/AbstractCurrencyTest.php
@@ -4,137 +4,137 @@ namespace KobensTest\Currency;
 
 use PHPUnit\Framework\TestCase;
 use Kobens\Currency\Exception\InvalidConstantException;
-use KobensTest\Currency\TestAsset\FooCurrency;
 use Kobens\Currency\AbstractCurrency;
 
 class AbstractCurrencyTest extends TestCase
 {
-    /**
-     * @var AbstractCurrency
-     */
-    protected $mock;
-
-    /**
-     * @var FooCurrency
-     */
-    protected $foo;
-
-    public function setup() : void
-    {
-        $this->mock = $this->getMockForAbstractClass(AbstractCurrency::class);
-        $this->foo = new FooCurrency();
-    }
 
     public function testGetCurrencyNameThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getCurrencyName();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getCurrencyName();
     }
 
     public function testGetCurrencyName() : void
     {
-        $this->assertEquals(
-            FooCurrency::CURRENCY_NAME,
-            $this->foo->getCurrencyName()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const CURRENCY_NAME = 'Conocybe';
+        };
+        $this->assertEquals('Conocybe', $anon->getCurrencyName());
     }
 
     public function testGetMainUnitNameThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getMainUnitName();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getMainUnitName();
     }
 
     public function testGetMainUnitName() : void
     {
-        $this->assertEquals(
-            FooCurrency::MAIN_UNIT,
-            $this->foo->getMainUnitName()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const MAIN_UNIT = 'Copelandia';
+        };
+        $this->assertEquals('Copelandia', $anon->getMainUnitName());
     }
 
     public function testGetMainUnitAbbreviationThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getMainUnitAbbreviation();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getMainUnitAbbreviation();
     }
 
     public function testGetMainUnitAbbreviation() : void
     {
-        $this->assertEquals(
-            FooCurrency::MAIN_UNIT_ABBREVIATION,
-            $this->foo->getMainUnitAbbreviation()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const MAIN_UNIT_ABBREVIATION = 'Galerina';
+        };
+        $this->assertEquals('Galerina', $anon->getMainUnitAbbreviation());
     }
 
     public function testGetSubunitNameThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getSubunitName();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getSubunitName();
     }
 
     public function testGetSubunitName() : void
     {
-        $this->assertEquals(
-            FooCurrency::SUB_UNIT,
-            $this->foo->getSubunitName()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const SUB_UNIT = 'Gymnopilus';
+        };
+        $this->assertEquals('Gymnopilus', $anon->getSubunitName());
     }
 
     public function testGetSubunitDenominationThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getSubunitDenomination();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getSubunitDenomination();
     }
 
     public function testGetSubunitDenomination() : void
     {
-        $this->assertEquals(
-            FooCurrency::DENOMINATION,
-            $this->foo->getSubunitDenomination()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const DENOMINATION = 80085;
+        };
+        $this->assertEquals(80085, $anon->getSubunitDenomination());
     }
 
     public function testGetCacheIdentifierThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getCacheIdentifier();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getCacheIdentifier();
     }
 
     public function testGetCacheIdentifier() : void
     {
-        $this->assertEquals(
-            FooCurrency::CACHE_IDENTIFIER,
-            $this->foo->getCacheIdentifier()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const CACHE_IDENTIFIER = 'Inocybe';
+        };
+        $this->assertEquals('Inocybe', $anon->getCacheIdentifier());
     }
 
     public function testGetPairIdentityThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getPairIdentity();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getPairIdentity();
     }
 
     public function testGetPairIdentity() : void
     {
-        $this->assertEquals(
-            FooCurrency::PAIR_IDENTIFIER,
-            $this->foo->getPairIdentity()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const PAIR_IDENTIFIER = 'Mycena';
+        };
+        $this->assertEquals('Mycena', $anon->getPairIdentity());
     }
 
     public function testGetCurrencyTypeThrowsException() : void
     {
         $this->expectException(InvalidConstantException::class);
-        $this->mock->getCurrencyType();
+        $anon = new class extends AbstractCurrency { };
+        $anon->getCurrencyType();
     }
 
     public function testGetCurrencyType() : void
     {
-        $this->assertEquals(
-            FooCurrency::CURRENCY_TYPE,
-            $this->foo->getCurrencyType()
-        );
+        $anon = new class extends AbstractCurrency
+        {
+            const CURRENCY_TYPE = 'Panaeolina';
+        };
+        $this->assertEquals('Panaeolina', $anon->getCurrencyType());
     }
-}
 
+}
