137c6ae15bce1e0d7284b87517bca59d859ffa65
Darren Felton <darren_felton@hotmail.com>
2019-02-24 18:24:36 -0600

    Removed docBlocks now redundant via type hinting

--- src/CurrencyInterface.php
+++ src/CurrencyInterface.php
@@ -7,60 +7,44 @@ interface CurrencyInterface
     /**
      * Return the full name of the currency.
      * Example: US Dollar | Bitcoin | Ethereaum
-     *
-     * @return string
      */
     public function getCurrencyName() : string;
 
     /**
      * Return the name of the currency's main unit.
      * Example: Dollar | Bitcoin | Ether
-     *
-     * @return string
      */
     public function getMainUnitName() : string;
 
     /**
      * Return the abbreviation for the main unit
      * Example: USD | BTC | ETH
-     *
-     * @return string
      */
     public function getMainUnitAbbreviation() : string;
 
     /**
      * Return the name of the currency's subunit.
      * Example: Cent | Satoshi | Wei
-     *
-     * @return string
      */
     public function getSubunitName() : string;
 
     /**
      * How many decimal points behind zero does the currency's subunit go.
-     *
-     * @return int
      */
     public function getScale() : int;
 
     /**
      * Return the string value to use in cache identifiers
-     *
-     * @return string
      */
     public function getCacheIdentifier() : string;
 
     /**
      * Return the string value to use for this currency in a pair
-     *
-     * @return string
      */
     public function getPairIdentity() : string;
 
     /**
      * Return the currency type (Fiat or Crypto)
-     *
-     * @return string
      */
     public function getCurrencyType() : string;
 }
