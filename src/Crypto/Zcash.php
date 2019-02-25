11c0f32d2a78d356d3b2a39fb1198afbc42d28d6
Darren Felton <2659558+dfelton@users.noreply.github.com>
2019-02-24 18:56:16 -0600

    Added Zcash

---
+++ src/Crypto/Zcash.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kobens\Currency\Crypto;
+
+class Zcash extends AbstractCrypto
+{
+    const CURRENCY_NAME          = 'Zcash';
+    const MAIN_UNIT              = 'Zcash';
+    const MAIN_UNIT_ABBREVIATION = 'ZEC';
+    const SUB_UNIT               = 'Zatoshi';
+    const DECIMAL_SCALE          = 8;
+    const PAIR_IDENTIFIER        = 'zec';
+    const CACHE_IDENTIFIER       = 'zec';
+}
+
